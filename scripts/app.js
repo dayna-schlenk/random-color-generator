8f648026376f128c544c7904463bc4dbf6855bb7
dayna-schlenk <dayna.schlenker@gmail.com>
2021-01-24 15:42:14 -0600

    Add js code for arrow functions

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,30 @@
-let square = (x) => {
-    return x * x;
-}
+let movies = [
+    {
+        title: 'Amadeaus',
+        score: 99
+    },
+    {
+        title: 'Stand By Me',
+        score: 85
+    },
+    {
+        title: 'Parasite',
+        score: 95
+    },
+    {
+        title: 'Alien',
+        score: 90
+    },
+]
+
+// let newMovies = movies.map(function(movie) {
+//     return `${movie.title} - ${movie.score / 10}`
+// })
+
+// let newMovies = movies.map((movie) => {
+//     return `${movie.title} - ${movie.score / 10}`
+// })
+
+let newMovies = movies.map(movie => (
+    `${movie.title} - ${movie.score / 10}`
+))
