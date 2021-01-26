dec6526c12ff60cc77ebe15d3af8427ca1b4acbf
dayna-schlenk <dayna.schlenker@gmail.com>
2021-01-26 06:41:16 -0600

    Practice with setTimeout and setInterval

--- scripts/app.js
+++ scripts/app.js
@@ -1,30 +1,3 @@
-let movies = [
-    {
-        title: 'Amadeaus',
-        score: 99
-    },
-    {
-        title: 'Stand By Me',
-        score: 85
-    },
-    {
-        title: 'Parasite',
-        score: 95
-    },
-    {
-        title: 'Alien',
-        score: 90
-    },
-]
-
-// let newMovies = movies.map(function(movie) {
-//     return `${movie.title} - ${movie.score / 10}`
-// })
-
-// let newMovies = movies.map((movie) => {
-//     return `${movie.title} - ${movie.score / 10}`
-// })
-
-let newMovies = movies.map(movie => (
-    `${movie.title} - ${movie.score / 10}`
-))
+let id = setInterval(() => {
+    console.log(Math.random())
+}, 2000);
