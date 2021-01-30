48a1f1fb9b4cdc93752883158762cdd7a3705638
dayna-schlenk <dayna.schlenker@gmail.com>
2021-01-30 09:51:40 -0600

    Destructure params

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,30 @@
-const bestBoy = {
-    name: 'Picasso',
-    age: 5,
-    veryGood: true,
-    weight: 65,
-    height: 'Mini pittie'
+const user = {
+    email: 'dayna.schlenker@gmail.com',
+    firstName: 'Dayna',
+    lastName: 'Schlenker',
+    born: 1993,
+    city: 'Bloomington',
+    state: "IL"
 };
 
-const bestGirl = {
-    name: 'Lady',
-    age: 8,
-    veryGood: true,
-};
+// Old way
+function fullName(user) {
+    return `${user.firstName} ${user.lastName}`
+}
 
-const {name, age, height = 'Big baby'} = bestGirl;
+// Returns "Dayna Schlenker"
+
+// New way, with destructuring
+function fullName(user) {
+    const {firstName, lastName} = user;
+    return `${firstName} ${lastName}`;
+}
+
+// Returns the same
+
+// Destructure on the way into the function - much shorter option
+function fullName({firstName, lastName}) {
+    return `${firstName} ${lastName}`;
+}
+
+// Returns the same
