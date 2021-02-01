d601db429301a552e9ce66f5a4a5bb66423d1d0f
dayna-schlenk <dayna.schlenker@gmail.com>
2021-02-01 06:15:47 -0600

    Add query selectors

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,6 @@
-document.querySelector("h2").innerHTML = "Bye!";
+document.getElementsByTagName("li")[2].style.color = "purple";
+
+document.querySelector("h1"); // tag
+document.querySelector("#title"); // id
+document.querySelector(".btn"); // class
+document.querySelector("li a"); // combined
