f91edffcdfdc4d55c2a5e60c246fa3aaef2edfbf
dayna-schlenk <dayna.schlenker@gmail.com>
2021-02-02 06:08:31 -0600

    Apply new css class to h1 through javascript

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,1 @@
-document.querySelector("button").classList.add("invisible)");
-
-// this adds a new class to the html, which we can then tap into on our stylesheets
+document.querySelector("h1").classList.add("huge");
