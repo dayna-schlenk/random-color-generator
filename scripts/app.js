e552b035c14acc3d726aa8369e38af4154432c00
dayna-schlenk <dayna.schlenker@gmail.com>
2021-02-21 12:38:24 -0600

    Change h1 color to pink with js

--- scripts/app.js
+++ scripts/app.js
@@ -3,3 +3,9 @@ let egg = document.querySelector('#egg');
 egg.setAttribute('src', 'https://www.flaticon.com/svg/static/icons/svg/3523/3523063.svg');
 
 egg.setAttribute('alt', 'chicken');
+
+
+// Changing styles
+let mainTitle = document.querySelector('h1');
+
+mainTitle.style.color = 'pink';
