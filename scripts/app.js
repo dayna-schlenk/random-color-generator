e250e8b0e905a6fa5c7da8101de634620a6b1b65
dayna-schlenk <dayna.schlenker@gmail.com>
2021-02-21 12:51:06 -0600

    Complete magical forest circle exercise

---
+++ scripts/app.js
@@ -0,0 +1,6 @@
+let mainPage = document.querySelector('#container');
+mainPage.style.textAlign = 'center';
+
+let treePic = document.querySelector('img');
+treePic.style.width = '150px';
+treePic.style.borderRadius = '50%';
