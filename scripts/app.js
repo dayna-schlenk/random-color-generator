2cce3beb368ebf842db41a3f1e1996df32baa436
dayna-schlenk <dayna.schlenker@gmail.com>
2021-02-23 06:23:47 -0600

    Go through classList exercises

---
+++ scripts/app.js
@@ -0,0 +1,8 @@
+let subTitle = document.querySelector('h2');
+
+subTitle.classList.toggle('purple');
+// on
+subTitle.classList.toggle('purple');
+// off
+subTitle.classList.toggle('purple');
+// on
