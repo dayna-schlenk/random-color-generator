b574922e2ae52344c63ef0869652abee1e3edb19
dayna-schlenk <dayna.schlenker@gmail.com>
2021-02-23 06:09:16 -0600

    Reset html and js files for next exercise

--- scripts/app.js
+++
@@ -1,9 +0,0 @@
-const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
-
-const spans = document.querySelectorAll('span');
-
-let count = 0;
-for (let span of spans) {
-    span.style.color = colors[count];
-    count++;
-}
