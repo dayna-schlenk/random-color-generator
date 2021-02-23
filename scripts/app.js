e5068deae0f1d0ef1309d0fbdc54f5c08130dcd3
dayna-schlenk <dayna.schlenker@gmail.com>
2021-02-23 06:25:57 -0600

    Reset code for new classList exercise

--- scripts/app.js
+++
@@ -1,8 +0,0 @@
-let subTitle = document.querySelector('h2');
-
-subTitle.classList.toggle('purple');
-// on
-subTitle.classList.toggle('purple');
-// off
-subTitle.classList.toggle('purple');
-// on
