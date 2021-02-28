5a16a21797ccef686b7587da9e50667b7cf27da4
dayna-schlenk <dayna.schlenker@gmail.com>
2021-02-28 15:40:20 -0600

    Solution 100 buttons exercise

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,13 @@
 // Create 100 new button elements with JS only
+
+// loop to create a button 100 times
+for (let i = 0; i < 100; i++) {
+    // Create new empty button element
+    let newBtn = document.createElement("button");
+
+    // Add text to the button
+    newBtn.innerText = "Hello";
+
+    // Add button to the container
+    document.getElementById("container").appendChild(newBtn);
+}
