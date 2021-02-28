f409eb09eea65c943c0e3e266ea7ff6005d48f35
dayna-schlenk <dayna.schlenker@gmail.com>
2021-02-28 15:28:30 -0600

    Prep for 100 button exercise

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,1 @@
-const newH3 = document.createElement("h3");
-// creates empty h3
-
-newH3.innerText = "I'm new here!";
-
-document.body.appendChild(newH3);
+// Create 100 new button elements with JS only
