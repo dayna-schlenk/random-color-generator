d222c18d4ec9eb4ae90dab7c99457ef3eea6d2fa
dayna-schlenk <dayna.schlenker@gmail.com>
2021-03-01 06:54:44 -0600

    Complete event listener exercise

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,5 @@
-const container = document.querySelector("#container");
-const baseURL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+let btn = document.querySelector('#v3');
 
-for (let i = 1; i <= 151; i++) {
-    const pokemon = document.createElement("div");
-    pokemon.classList.add("pokemon");
-    const label = document.createElement("span")
-    label.innerText = `#${i}`;
-
-    const newImg = document.createElement("img");
-    newImg.src = `${baseURL}${i}.png`;
-    pokemon.appendChild(newImg);
-    pokemon.appendChild(label);
-    container.appendChild(pokemon);
-}
+btn.addEventListener('mouseup', function() {
+    alert("CLICKED!");
+})
