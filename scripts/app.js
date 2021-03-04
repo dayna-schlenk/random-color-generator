c2be2df84e2415564adabb83d01114d22f72bdad
dayna-schlenk <dayna.schlenker@gmail.com>
2021-03-04 07:03:11 -0600

    Apply random number to rgb sequence

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,8 @@
 // Generates random number between 0 and 255
-console.log(Math.floor(Math.random() * (255 - 0) + 0));
+function randomNum() {
+    return Math.floor(Math.random() * (255 - 0) + 0);
+}
+
+// Applies random number to rgb sequence
+let newRgb = `rgb(${randomNum()}, ${randomNum()}, ${randomNum()})`;
+console.log(newRgb);
